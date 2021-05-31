70e702e0b33b0ea86dbad66356e9689ada787349
amapy20 <engineer.ama.2021@gmail.com>
2021-05-31 18:55:34 +0300

    our-service-section

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,5 @@ import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
+import "@fortawesome/fontawesome-free/js/all";
 import "../sass/style.scss";
