18b00d8daf6ec6fc32d752d9887f277bac3b2ddd
amapy20 <engineer.ama.2021@gmail.com>
2021-06-01 18:56:53 +0300

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // import 'bootstrap';
 import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
-import "@laylazi/bootstrap-rtl-scss/scss/bootstrap-rtl.scss";
+// import "@laylazi/bootstrap-rtl-scss/scss/bootstrap-rtl.scss";
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import "@fortawesome/fontawesome-free/js/all";
